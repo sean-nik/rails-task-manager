b792992ca132350125910316abc784b64476e13f
Sean Nikravesh <seannik@bluewin.ch>
2018-02-20 15:27:03 +0900

    CRUD features added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'tasks/new', to: 'tasks#new', as: 'new'
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update', as: 'update_task'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
